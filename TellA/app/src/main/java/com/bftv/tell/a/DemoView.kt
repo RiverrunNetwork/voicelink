46e136a6fe9935a83cb393c3606394745a7d8d44
renyang <renyang@bftv.com>
2017-11-20 20:20:57 +0800

    update

--- TellA/app/src/main/java/com/bftv/tell/a/DemoView.kt
+++ TellA/app/src/main/java/com/bftv/tell/a/DemoView.kt
@@ -6,14 +6,14 @@ import android.os.Handler
 import android.os.Looper
 import android.view.View
 import android.widget.Toast
-import com.bftv.fui.constantplugin.TellCode.TELL_APP_CACHE
+import com.bftv.fui.constantplugin.SequenceCode
+import com.bftv.fui.constantplugin.TellCode.*
 import com.bftv.fui.tell.Tell
 import com.bftv.fui.tell.TellManager
 import com.bftv.fui.thirdparty.InterceptionData
 import com.bftv.fui.thirdparty.VoiceFeedback
 import com.bftv.fui.thirdparty.notify.DataChange
 import com.bftv.fui.thirdparty.notify.IVoiceObserver
-import com.bftv.tell.a.R.id.btn_app
 import kotlinx.android.synthetic.main.demo_layout.*
 import java.util.HashMap
 
@@ -39,6 +39,45 @@ class DemoView : Activity(), IVoiceObserver {
             tell.pck = packageName
             tell.tellType = TELL_APP_CACHE
             TellManager.getInstance().tell(App.sApp, tell)
+        })
+
+        //界面指令词
+        btn_view.setOnClickListener(View.OnClickListener {
+            val tell = Tell()
+            val hashMap = HashMap<String,String>()
+            hashMap.put("播放","playTag")
+            tell.viewCacheMap = hashMap
+            tell.pck = packageName
+            tell.className = this@DemoView.javaClass.name
+            tell.tellType = TELL_VIEW_CACHE
+            TellManager.getInstance().tell(App.sApp, tell)
+        })
+
+        //追加界面指令词
+        btn_view_append.setOnClickListener(View.OnClickListener {
+            val tell = Tell()
+            val hashMap = HashMap<String,String>()
+            hashMap.put("收藏","collectTag")
+            tell.viewCacheMap = hashMap
+            tell.pck = packageName
+            tell.isAppend = true
+            tell.tellType = TELL_VIEW_CACHE
+            TellManager.getInstance().tell(App.sApp, tell)
+        })
+
+        //系统指令词
+        btn_system_app.setOnClickListener(View.OnClickListener {
+            val tell = Tell()
+            tell.pck = packageName
+            tell.className = this@DemoView.javaClass.name
+            tell.tellType = TELL_SYSTEM
+            tell.sequencecode = SequenceCode.TYPE_PAGE
+            TellManager.getInstance().tell(App.sApp, tell)
+        })
+
+        //拉起语音
+        btn_pull_far.setOnClickListener(View.OnClickListener {
+            TellManager.getInstance().farPull(App.sApp,packageName)
         })
     }
 
