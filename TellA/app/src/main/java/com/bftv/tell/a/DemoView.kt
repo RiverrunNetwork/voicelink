79b83fdac6548d39b8ba333058166ca4a6d1c6b5
renyang <renyang@bftv.com>
2018-02-23 15:31:09 +0800

    add
    Signed-off-by: renyang <renyang@bftv.com>

--- TellA/app/src/main/java/com/bftv/tell/a/DemoView.kt
+++ TellA/app/src/main/java/com/bftv/tell/a/DemoView.kt
@@ -158,7 +158,39 @@ class DemoView : Activity(), IVoiceObserver {
             tell.pck = packageName
             tell.className = this@DemoView.javaClass.name
             tell.tellType = TELL_ASR
-            tell.isNeedPinYin = true
+            TellManager.getInstance().tell(App.sApp, tell)
+        })
+
+        //接管语音的ASR,并且不显示语音动画
+        btn_asr_hide_animation.setOnClickListener(View.OnClickListener {
+            val tell = Tell()
+            tell.pck = packageName
+            tell.className = this@DemoView.javaClass.name
+            tell.tellType = TELL_ASR
+            tell.isHideAnimation = true
+            TellManager.getInstance().tell(App.sApp, tell)
+        })
+
+        //接管语音的ASR,并且不显示语音动画,并且使用游戏引擎
+        btn_asr_hide_animation_ddz_game.setOnClickListener(View.OnClickListener {
+            val tell = Tell()
+            tell.pck = packageName
+            tell.className = this@DemoView.javaClass.name
+            tell.tellType = TELL_ASR
+            tell.isHideAnimation = true
+            tell.isNeedDDZGameConversion = true
+            TellManager.getInstance().tell(App.sApp, tell)
+        })
+
+        //接管语音的ASR,并且不显示语音动画,并且使用游戏引擎,并且开启连续识别
+        btn_asr_hide_animation_ddz_game_continuous_recognition.setOnClickListener(View.OnClickListener {
+            val tell = Tell()
+            tell.pck = packageName
+            tell.className = this@DemoView.javaClass.name
+            tell.tellType = TELL_ASR
+            tell.isHideAnimation = true
+            tell.isNeedDDZGameConversion = true
+            tell.isEnableContinuousRecognition = true
             TellManager.getInstance().tell(App.sApp, tell)
         })
 
